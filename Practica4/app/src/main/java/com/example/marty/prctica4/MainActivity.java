6137d91258730963e9aa30200be1779a8656e25a
MYepiz <myepiz96@gmail.com>
2017-08-29 08:24:13 -0600

    subir practica 4 toast

--- Practica4/app/src/main/java/com/example/marty/prctica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/marty/prctica4/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.marty.prctica4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(this, "Un Tostón", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
