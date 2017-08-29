745d4c7c412e0d2a60914f838ec33a0147194d47
MYepiz <myepiz96@gmail.com>
2017-08-29 09:10:44 -0600

    Toast de sodas gratis

--- Practica4/app/src/main/java/com/example/marty/prctica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/marty/prctica4/MainActivity.java
@@ -13,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
         String nombre ="Raul Alfonso";
         String apellido = "Tejada Castañeda";
         //String completo = String.join(" ", apellido, nombre);
-        Toast toast = Toast.makeText(this, nombre+" "+apellido, Toast.LENGTH_LONG);
-        toast.show();
+        //Toast toast = Toast.makeText(this, nombre+" "+apellido, Toast.LENGTH_LONG);
+        //toast.show();
+
+        int torta = 40;
+
+        int compra = 3;
+
+        if (compra*torta > 80){
+            Toast T = Toast.makeText(this, "Soda Gratis WUUU!", Toast.LENGTH_LONG);
+            T.show();
+        }else{
+            Toast M = Toast.makeText(this, "Paguele men!", Toast.LENGTH_LONG);
+            M.show();
+        }
     }
 }
